66b237be0b3c2ac3ed91e4735fe8fc0fc67ac8a5
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2026-02-02 17:16:07 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O ~/.local/bin/loopduck https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Loop-Duck/main/Loopduck.py
+chmod +x ~/.local/bin/loopduck
+in -s ~/.local/bin/loopduck ~/.local/bin/loop
