ea8a65b4a2e7068f8ddd8bbfe21432daff39497f
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2026-02-02 21:37:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ chmod +x ~/.local/bin/loopduck
 cd ~/.local/bin
 ln -s loopduck Loop
 ln -s loopduck loop
+wget -O ~/.local/share/icons/retro/loopduck.png https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Loop-Duck/main/Icon.png
